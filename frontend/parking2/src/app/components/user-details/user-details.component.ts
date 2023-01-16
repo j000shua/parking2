9f1f80915d1f11351d9adf08397f627821317eca
j000shua <gemyetmems@gmail.com>
2023-01-16 12:04:18 +0100

    some frontend, can display the list of users

--- frontend/parking2/src/app/components/user-details/user-details.component.ts
+++ frontend/parking2/src/app/components/user-details/user-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-details',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class UserDetailsComponent {
 
+  user?: User;
+
+  constructor(private userService: UserService) {}
+
+  retrieveUser(): void {
+    this.userService.get(1)
+      .subscribe({
+        next: (data) => {
+          this.user = data;
+          console.log(data);
+        },
+        error: (e) => console.error(e)
+      })
+  }
 }
